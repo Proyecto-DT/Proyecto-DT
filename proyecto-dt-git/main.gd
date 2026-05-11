f0c868c7276510891f1a084538140efee8896735
Colan-Z <francoivancolantonio@gmail.com>
2026-05-11 20:15:15 -0300

    feature and fix: addon state, loops, enemigos. arreglo generación del mapa.
    Se agrego enemigo hormiga para hacer pruebas, se agrego el addon state para la hormiga (WIP) , el camino ahora genera loops procedurales.
    Se arreglaron las coordenadas de los caminos, la latitud y longitud. Estaba todo al revés.

--- proyecto-dt-git/main.gd
+++ proyecto-dt-git/main.gd
@@ -4,48 +4,85 @@ extends Node3D
 @export var tile_final:PackedScene
 @export var tile_straight:PackedScene
 @export var tile_esquina:PackedScene
-@export var tile_cesped:PackedScene
+@export var tile_cruce:PackedScene
+@export var tile_cesped:Array[PackedScene]
+@export var tile_enemigo:PackedScene
 
+#Tamaño del mapa
 @export var mapa_longitud:int = 16
-@export var mapa_latitud:int = 9
+@export var mapa_latitud:int = 10
+
+#Restricciones del camino
+@export var min_path_size = 50
+@export var max_path_size = 70
+@export var min_loops = 3
+@export var max_loops = 5
  
 var _generadorcaminos:GeneradorCaminos
 
-func _ready():
+func _ready(): 
 	_generadorcaminos = GeneradorCaminos.new(mapa_latitud, mapa_longitud)
 	_mostrar_camino()
 	_completar_mapa()
 
-func _completar_mapa():
+	await get_tree().create_timer(2).timeout
+	_pop_along_grid()
+
+func _add_curve_point(c3d:Curve3D, v3:Vector3) ->bool: #agrega curvas
+	c3d.add_point(v3)
+	return true
+
+func _pop_along_grid(): #spawn hormiga
+	var hormiga = tile_enemigo.instantiate()
+	var c3d:Curve3D = Curve3D.new()
+	
+	for elemento in _generadorcaminos.obtener_camino():
+		c3d.add_point(Vector3(elemento.x, 0.1, elemento.y))
+	
+	var p3d:Path3D = Path3D.new()
+	add_child(p3d)
+	p3d.curve = c3d
+	
+	var pf3d:PathFollow3D = PathFollow3D.new()
+	p3d.add_child(pf3d)
+	pf3d.add_child(hormiga)
+	
+	var distancia_actual:float = 0.0
+	
+	while distancia_actual < c3d.point_count-1:
+		distancia_actual += 0.02
+		pf3d.progress = clamp(distancia_actual, 0, c3d.point_count-1.00001)
+		await get_tree().create_timer(0.01).timeout	
+	
+func _completar_mapa(): #genera los tiles alrededor del camino
 	for x in range(mapa_longitud):
 		for y in range(mapa_latitud):
 			if not _generadorcaminos.obtener_camino().has(Vector2i(x,y)):
-				var tile:Node3D = tile_cesped.instantiate()
+				var tile:Node3D = tile_cesped.pick_random().instantiate()
 				add_child(tile)
-				tile.global_position = Vector3(x, 0, y)
+				tile.position = Vector3(x, 0, y)
+				tile.rotation_degrees = Vector3(0, randi_range(0,3)*90, 0)
 
-func _mostrar_camino():
-	var _camino:Array[Vector2i] = _generadorcaminos.generar_camino()
-	while _camino.size() < 35:
-		_camino = _generadorcaminos.generar_camino()
+func _mostrar_camino(): 
+	var _iteration_count:int = 1
+	_generadorcaminos.generar_camino(true)
+	
+	while (_generadorcaminos.obtener_camino().size() < min_path_size or _generadorcaminos.obtener_camino().size() > max_path_size or _generadorcaminos.get_loop_count() < min_loops or _generadorcaminos.get_loop_count() > max_loops):
+		_iteration_count += 1
+		_generadorcaminos.generar_camino(true)
 
-	print(_camino)
-	for elemento in _camino:
-		var tile_score:int = _generadorcaminos.get_tile_score(elemento)
+		print("Se generó una ruta de %d baldosas después de %d iteraciones" % [_generadorcaminos.obtener_camino().size(), _iteration_count])
+		print(_generadorcaminos.obtener_camino())
 
-		var tile: Node3D = tile_cesped.instantiate()
+	for i in range(_generadorcaminos.obtener_camino().size()):
+		var tile_score:int = _generadorcaminos.get_tile_score(i)
+		var tile:Node3D = tile_cesped[0].instantiate()
 		var tile_rotation: Vector3 = Vector3.ZERO
 
-		if tile_score == 2:
-			tile = tile_inicio.instantiate()
-			tile_rotation = Vector3(0,90,0)
-		if tile_score == 8:
-			tile = tile_final.instantiate()
-			tile_rotation = Vector3(0,90,0)
-		if tile_score == 10:
+		if tile_score == 5:
 			tile = tile_straight.instantiate()
 			tile_rotation = Vector3(0,90,0)
-		elif tile_score == 1 or tile_score == 4 or tile_score == 5:
+		elif tile_score == 10:
 			tile = tile_straight.instantiate()
 			tile_rotation = Vector3(0,0,0)
 		elif tile_score == 6:
@@ -53,16 +90,29 @@ func _mostrar_camino():
 			tile_rotation = Vector3(0,0,0)
 		elif tile_score == 12:
 			tile = tile_esquina.instantiate()
-			tile_rotation = Vector3(0,270,0)
+			tile_rotation = Vector3(0,90,0)
 		elif tile_score == 9:
 			tile = tile_esquina.instantiate()
 			tile_rotation = Vector3(0,180,0)
 		elif tile_score == 3:
 			tile = tile_esquina.instantiate()
-			tile_rotation = Vector3(0,90,0)
+			tile_rotation = Vector3(0,270,0)
+		elif tile_score == 7 or tile_score == 11 or tile_score == 13 or tile_score == 14 or tile_score == 15:
+			tile = tile_cruce.instantiate()
+			tile_rotation = Vector3(0, 0, 0)
+		elif tile_score == 1:
+			tile = tile_final.instantiate()
+			tile_rotation = Vector3(0, -90, 0)
+		elif tile_score == 4:
+			tile = tile_inicio.instantiate()
+			tile_rotation = Vector3(0, 90, 0)
+		else:
+			# fallback
+			tile = tile_cesped[0].instantiate()
 		print (tile_score)
 
+
 		add_child(tile)
-		tile.global_position = Vector3(elemento.x, 0, elemento.y)
-		tile.global_rotation_degrees = tile_rotation
+		tile.position = Vector3(_generadorcaminos.get_path_tile(i).x, 0, _generadorcaminos.get_path_tile(i).y)
+		tile.rotation_degrees = tile_rotation
 		
--- proyecto-dt-git/scripts/generador_caminos.gd
+++ proyecto-dt-git/scripts/generador_caminos.gd
@@ -1,8 +1,9 @@
 extends Object
 class_name GeneradorCaminos
 
-var _cuadricula_longitud:int = 16
-var _cuadricula_latitud:int = 9
+var _cuadricula_longitud:int
+var _cuadricula_latitud:int
+var _loop_count:int
 
 var _camino: Array[Vector2i]
 
@@ -10,8 +11,10 @@ func _init(longitud:int,latitud:int):
 	_cuadricula_latitud = latitud
 	_cuadricula_longitud = longitud
 
-func generar_camino(): 
-	_camino.clear() #Borra el camino anterior, si existia alguno.
+func generar_camino(add_loops:bool = false):
+	_camino.clear()
+	_loop_count = 0
+	randomize()
 	
 	var x = 0 
 	var y = int(_cuadricula_longitud/2)
@@ -30,19 +33,130 @@ func generar_camino():
 		elif choice == 2 and y > 1 and not _camino.has(Vector2i(x, y-1)):
 			y -= 1
 		
+	if add_loops:
+		_add_loops()
+		
 	return _camino
 
-func get_tile_score(tile: Vector2i):
+func get_tile_score(index:int) -> int:
 	var score:int = 0
-	var x = tile.x
-	var y = tile.y
+	var x = _camino[index].x
+	var y = _camino[index].y
 	
-	score += 1 if _camino.has(Vector2(x,y-1)) else 0 #Score 1 = camino arriba
-	score += 2 if _camino.has(Vector2(x+1,y)) else 0 #Score 2 = Camino derecha
-	score += 4 if _camino.has(Vector2(x,y+1)) else 0 #Score 4 = Camino abajo
-	score += 8 if _camino.has(Vector2(x-1,y)) else 0 #Score 8 = camino izquierda
+	score += 1 if _camino.has(Vector2i(x-1,y)) else 0 # arriba
+	score += 2 if _camino.has(Vector2i(x,y+1)) else 0 # derecha
+	score += 4 if _camino.has(Vector2i(x+1,y)) else 0 # abajo
+	score += 8 if _camino.has(Vector2i(x,y-1)) else 0 # izquierda
 	
 	return score
 	
 func obtener_camino() -> Array[Vector2i]:
 	return _camino
+	
+func _add_loops():
+	#  agrega loops
+	var loops_generated:bool = true
+	
+	#genera loops hasta que no pueda mas
+	
+	while loops_generated:
+		loops_generated = false
+		for i in range(_camino.size()):
+			var loop:Array[Vector2i] = _is_loop_option(i)
+			#si el tamaño del loop es >0, entonces _is_loop_option encontro el loop y lo agregó
+			if loop.size() > 0:
+				loops_generated = true
+				for j in range(loop.size()):
+					if not _camino.has(loop[j]):
+						_camino.insert(i+1+j, loop[j])
+	var safety := 0
+
+	while loops_generated and safety < 100:
+		safety += 1
+
+func _is_loop_option(index:int) -> Array[Vector2i]:
+	var x: int = _camino[index].x
+	var y: int = _camino[index].y
+	var return_camino:Array[Vector2i]
+
+	#Amarillo
+	if (x < _cuadricula_latitud-3 and y > 2
+		and _tile_loc_free(x, y-3) and _tile_loc_free(x+1, y-3) and _tile_loc_free(x+2, y-3)
+		and _tile_loc_free(x-1, y-2) and _tile_loc_free(x, y-2) and _tile_loc_free(x+1, y-2) and _tile_loc_free(x+2, y-2) and _tile_loc_free(x+3, y-2)
+		and _tile_loc_free(x-1, y-1) and _tile_loc_free(x, y-1) and _tile_loc_free(x+1, y-1) and _tile_loc_free(x+2, y-1) and _tile_loc_free(x+3, y-1)
+		and _tile_loc_free(x+1,y) and _tile_loc_free(x+2,y) and _tile_loc_free(x+3,y)
+		and _tile_loc_free(x+1,y+1) and _tile_loc_free(x+2,y+1)):
+		return_camino = [Vector2i(x+1,y), Vector2i(x+2,y), Vector2i(x+2,y-1), Vector2i(x+2,y-2), Vector2i(x+1,y-2), Vector2i(x,y-2), Vector2i(x,y-1)]
+
+		if index > 0 and _camino[index-1].y > y:
+			return_camino.reverse()
+			
+		_loop_count += 1
+		return_camino.append(Vector2i(x,y))
+
+	#Azul
+	elif (x > 2 and y > 1
+			and _tile_loc_free(x, y-3) and _tile_loc_free(x-1, y-3) and _tile_loc_free(x-2, y-3)		
+			and _tile_loc_free(x-1, y) and _tile_loc_free(x-2, y) and _tile_loc_free(x-3, y)
+			and _tile_loc_free(x+1, y-1) and _tile_loc_free(x, y-1) and _tile_loc_free(x-2, y-1) and _tile_loc_free(x-3, y-1)
+			and _tile_loc_free(x+1, y-2) and _tile_loc_free(x, y-2) and _tile_loc_free(x-1, y-2) and _tile_loc_free(x-2, y-2) and _tile_loc_free(x-3, y-2)
+			and _tile_loc_free(x-1, y+1) and _tile_loc_free(x-2, y+1)):
+		return_camino = [Vector2i(x,y-1), Vector2i(x,y-2), Vector2i(x-1,y-2), Vector2i(x-2,y-2), Vector2i(x-2,y-1), Vector2i(x-2,y), Vector2i(x-1,y)]
+
+		if index > 0 and _camino[index-1].x > x:
+			return_camino.reverse()
+
+		_loop_count += 1
+		return_camino.append(Vector2i(x,y))
+	#Rojo
+	elif (x < _cuadricula_latitud-3 and y < _cuadricula_longitud-3
+			and _tile_loc_free(x, y+3) and _tile_loc_free(x+1, y+3) and _tile_loc_free(x+2, y+3)		
+			and _tile_loc_free(x+1, y-1) and _tile_loc_free(x+2, y-1)
+			and _tile_loc_free(x+1, y) and _tile_loc_free(x+2, y) and _tile_loc_free(x+3, y)
+			and _tile_loc_free(x-1, y+1) and _tile_loc_free(x, y+1) and _tile_loc_free(x+2, y+1) and _tile_loc_free(x+3, y+1)
+			and _tile_loc_free(x-1, y+2) and _tile_loc_free(x, y+2) and _tile_loc_free(x+1, y+2) and _tile_loc_free(x+2, y+2) and _tile_loc_free(x+3, y+2)):
+		return_camino = [Vector2i(x+1,y), Vector2i(x+2,y), Vector2i(x+2,y+1), Vector2i(x+2,y+2), Vector2i(x+1,y+2), Vector2i(x,y+2), Vector2i(x,y+1)]
+
+		if index > 0 and _camino[index-1].y < y:
+			return_camino.reverse()
+		
+		_loop_count += 1
+		return_camino.append(Vector2i(x,y))
+	# Marron
+	elif (x > 2 and y < _cuadricula_longitud-3
+			and _tile_loc_free(x, y+3) and _tile_loc_free(x-1, y+3) and _tile_loc_free(x-2, y+3)
+			and _tile_loc_free(x-1, y-1) and _tile_loc_free(x-2, y-1)
+			and _tile_loc_free(x-1, y) and _tile_loc_free(x-2, y) and _tile_loc_free(x-3, y)
+			and _tile_loc_free(x+1, y+1) and _tile_loc_free(x, y+1) and _tile_loc_free(x-2, y+1) and _tile_loc_free(x-3, y+1)
+			and _tile_loc_free(x+1, y+2) and _tile_loc_free(x, y+2) and _tile_loc_free(x-1, y+2) and _tile_loc_free(x-2, y+2) and _tile_loc_free(x-3, y+2)):
+		return_camino = [Vector2i(x,y+1), Vector2i(x,y+2), Vector2i(x-1,y+2), Vector2i(x-2,y+2), Vector2i(x-2,y+1), Vector2i(x-2,y), Vector2i(x-1,y)]
+
+		if index > 0 and _camino[index-1].x > x:
+			return_camino.reverse()
+		
+		_loop_count += 1
+		return_camino.append(Vector2i(x,y))
+		
+	return return_camino
+	
+## Returns true if there is a path tile at the x,y coordinate.
+func _tile_loc_taken(x: int, y: int) -> bool:
+	return _camino.has(Vector2i(x,y))
+	
+## Returns true if there is no path tile at the x,y coordinate.
+func _tile_loc_free(x: int, y: int) -> bool:
+	if x < 0 or x >= _cuadricula_latitud:
+		return false
+
+	if y < 0 or y >= _cuadricula_longitud:
+		return false
+
+	return not _camino.has(Vector2i(x,y))
+
+## Returns the number of loops currently in the path.
+func get_loop_count() -> int:
+	return _loop_count
+
+## Returns the Vector2i path tile at the given index.
+func get_path_tile(index:int) -> Vector2i:
+	return _camino[index]
