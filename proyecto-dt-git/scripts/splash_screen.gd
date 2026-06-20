16d187cc8bda4f4abc367af4d47867a43efe9c59
NicoPelleritti <nico-1997@outlook.com>
2026-06-20 16:13:30 -0300

    Add Splash_Screen
    Se agrego splash screen pero hay que cambiar el logo.

---
+++ proyecto-dt-git/scripts/splash_screen.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@export var siguiente_escena: PackedScene = preload("res://scenes/menu/menu.tscn")
+var duracion_fade_in: float = 1.0
+var tiempo_espera: float = 1.0
+var duracion_fade_out: float = 1.0
+
+@onready var logo = $ColorRect/CenterContainer/TextureRect
+
+func _ready():
+	fade()
+
+func fade():
+	logo.modulate.a = 0.0
+	
+	var tween = create_tween()
+	tween.set_parallel(false)
+	
+	tween.tween_property(logo, "modulate:a", 1.0, duracion_fade_in)
+	tween.tween_interval(tiempo_espera)
+	tween.tween_property(logo, "modulate:a", 0.0, duracion_fade_out)
+	
+	await tween.finished
+	get_tree().change_scene_to_packed(siguiente_escena)
